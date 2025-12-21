104e36fd9d47c1fb279a38b9c3f08b459f9e5302
barakis <lokislayer@gmail.com>
2025-12-21 00:31:17 -0600

    Trimmed and changed some of the phrases.

--- Interactive/Questions.cs
+++ Interactive/Questions.cs
@@ -4,11 +4,11 @@
     {
         public readonly static string about = "Genesys Dice Roller written in .NET CORE by Matthew Barakis Brown, @2025";
         public readonly static string copyright = "Genesys RPG created by Fantasy Flight Games @2025";
-        public readonly static string question1 = "How many boost dice? (return for 0) => ";
-        public readonly static string question2 = "How many setback dice (return for 0) => ";
-        public readonly static string question3 = "How many Ability dice (return for 0) => ";
-        public readonly static string question4 = "How many Difficulty dice (return for 0) => ";
-        public readonly static string question5 = "How many Proficiency dice (return for 0) => ";
-        public readonly static string question6 = "How many Challenge dice (return for 0) => ";
+        public readonly static string question1 = "BOOST (return for 0) => ";
+        public readonly static string question2 = "SETBACK (return for 0) => ";
+        public readonly static string question3 = "Ability (return for 0) => ";
+        public readonly static string question4 = "Difficulty (return for 0) => ";
+        public readonly static string question5 = "Proficiency (return for 0) => ";
+        public readonly static string question6 = "Challenge (return for 0) => ";
     }
 }
