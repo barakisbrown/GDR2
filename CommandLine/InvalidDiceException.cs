c6011a26a8561a01f60c0802f8e270f85e342208
barakis <lokislayer@gmail.com>
2025-12-20 18:26:53 -0600

    added new exception for invalid dice being used.

---
+++ CommandLine/InvalidDiceException.cs
@@ -0,0 +1,21 @@
+﻿using Spectre.Console;
+
+namespace CommandLine
+{
+    public class InvalidDiceException : Exception
+    {
+        public InvalidDiceException() 
+        {
+            AnsiConsole.MarkupLine("[RED]ERROR[/] has occured. Wrong Color and Illegal Charater was entered.");
+            AnsiConsole.WriteLine("Please check that you entered is legal.");
+        }
+
+        public InvalidDiceException(string message) : base(message)
+        {
+        }
+
+        public InvalidDiceException(string message, Exception inner)
+            : base(message, inner)
+        { }
+    }
+}
