70e2dc0757a7c048f6e681169f485dfc7e96799a
Matthew Brown <lokislayer@gmail.com>
2025-12-27 21:13:45 -0600

    Add new logic for looping and other logic issues resolved.

--- Interactive/App.cs
+++ Interactive/App.cs
@@ -7,33 +7,57 @@ namespace Interactive
 {
     public class App
     {
-        private Pool _Dice = new Pool();
-        private Dictionary<Colors, int> scores = new Dictionary<Colors, int>();
+        private readonly Pool _dice = new Pool();
+        private Dictionary<Colors, int> _scores = new Dictionary<Colors, int>();
         public App()
         {
-            AnsiConsole.Clear();
-            AnsiConsole.MarkupLineInterpolated($"[underline Red]{Questions.ABOUT} [/]");
-            AnsiConsole.MarkupLineInterpolated($"[red]{Questions.COPYRIGHT}[/]");
-            AnsiConsole.WriteLine();
-            // Set Scores to number of dice rolled if NOT 0
-            scores.Add(Colors.BLUE, askQuestion(Questions.BOOST));
-            scores.Add(Colors.BLACK, askQuestion(Questions.SETBACK));
-            scores.Add(Colors.GREEN, askQuestion(Questions.ABILITY));
-            scores.Add(Colors.PURPLE, askQuestion(Questions.DIFFICULTY));
-            scores.Add(Colors.YELLOW, askQuestion(Questions.PROFICENCY));
-            scores.Add(Colors.RED, askQuestion(Questions.CHALLENGE));
-            // ADD Scores to the Dice Pool
-            foreach(var score in scores)
+           RUNNER();
+        }
+
+        private void RUNNER()
+        {
+            while (true)
             {
-                if (score.Value != 0)
+                AnsiConsole.Clear();
+                AnsiConsole.MarkupLineInterpolated($"[underline Red]{Questions.ABOUT} [/]");
+                AnsiConsole.MarkupLineInterpolated($"[red]{Questions.COPYRIGHT}[/]");
+                AnsiConsole.WriteLine();
+                // Set Scores to number of dice rolled if NOT 0
+                _scores.Add(Colors.BLUE, AskQuestion(Questions.BOOST));
+                _scores.Add(Colors.BLACK, AskQuestion(Questions.SETBACK));
+                _scores.Add(Colors.GREEN, AskQuestion(Questions.ABILITY));
+                _scores.Add(Colors.PURPLE, AskQuestion(Questions.DIFFICULTY));
+                _scores.Add(Colors.YELLOW, AskQuestion(Questions.PROFICENCY));
+                _scores.Add(Colors.RED, AskQuestion(Questions.CHALLENGE));
+                // ADD Scores to the Dice Pool
+                foreach (var score in _scores)
                 {
-                    _Dice.Add(ColorToDice(score.Key), score.Value);
+                    if (score.Value != 0)
+                    {
+                        _dice.Add(ColorToDice(score.Key), score.Value);
+                    }
                 }
+
+                // ROLL THEM
+                AnsiConsole.WriteLine();
+                if (_dice.COUNT == 0)
+                {
+                    AnsiConsole.MarkupLine("[blink red]ERROR : NO DICE ENTERED. ENTER DICE TO SEE RESULTS![/]");
+                }
+                else
+                {
+                    _dice.Roll();
+                    DisplayResults();
+                }
+
+                if (YESNO())
+                {
+                    _dice.Clear();
+                    _scores.Clear();
+                    continue;
+                }
+                break;
             }
-            // ROLL THEM
-            AnsiConsole.WriteLine();
-            _Dice.Roll();
-            DisplayResults();
         }
 
         private void DisplayResults()
@@ -49,33 +73,49 @@ namespace Interactive
             table = addColumn(table, "DESPAIRS");
             // ROWS
             table.AddRow(
-                _Dice.SUCCESSES.ToString(),
-                _Dice.Failures.ToString(),
-                _Dice.ADVANTAGES.ToString(),
-                _Dice.THREATS.ToString(),
-                _Dice.TRIUMPS.ToString(),
-                _Dice.DESPAIR.ToString());
+                _dice.SUCCESSES.ToString(),
+                _dice.Failures.ToString(),
+                _dice.ADVANTAGES.ToString(),
+                _dice.THREATS.ToString(),
+                _dice.TRIUMPS.ToString(),
+                _dice.DESPAIR.ToString());
             AnsiConsole.Write(table);
             AnsiConsole.WriteLine();
-            int net = _Dice.NETSUCCESSES;
-            int adv = _Dice.NETADVANTAGES;
-            AnsiConsole.Write("Did the take that was rolled PASS OR FAIL?  ");
+            // CHECK TO MAKE SURE NO DICE WAS ROLLED
+            var net = _dice.NETSUCCESSES;
+            var adv = _dice.NETADVANTAGES;
+            AnsiConsole.Write("Did the task that was rolled PASS OR FAIL?  ");
             if (net > 0)
-                AnsiConsole.Markup("[WHITE]PASSED[/]");
+                AnsiConsole.Markup("[BOLD]PASSED[/]");
             else
-                AnsiConsole.Markup("[RED]FAILES[/]");
+                AnsiConsole.Markup("[RED]FAILED[/]");
+
             // ADDITIONAL INFO
             AnsiConsole.Markup("  WITH ");
             if (adv > 0)
-                AnsiConsole.MarkupLineInterpolated($"[WHITE]{adv} Advantages for player to use![/]");
+                AnsiConsole.MarkupLineInterpolated($"[BOLD]{adv} Advantages for player to use![/]");
             else if (adv == 0)
-                AnsiConsole.MarkupLineInterpolated($"[WHITE] NO advantages or Threats rolled[/]");
+                AnsiConsole.MarkupLineInterpolated($"[BOLD] NO advantages or Threats rolled[/]");
             else
                 AnsiConsole.MarkupLineInterpolated($"[RED]{adv} THREATS for GM to use against you![/]");
+            // ADDING LOGIC just in case Despair or Triump has been rolled
+            if (_dice.DESPAIR > 0 || _dice.TRIUMPS > 0)
+            {
+                var despair = (_dice.DESPAIR > 0) ? $"You have {_dice.DESPAIR} AGAINST YOU" : string.Empty;
+                var triump = (_dice.TRIUMPS > 0) ? $"You have {_dice.TRIUMPS} FOR YOU" : string.Empty;
+                if (despair != string.Empty)
+                {
+                    AnsiConsole.MarkupLineInterpolated($"[RED]{despair}[/]");
+                }
+                else if (triump != string.Empty)
+                {
+                    AnsiConsole.MarkupLineInterpolated($"[BOLD]{triump}[/]");
+                }
+            }
             AnsiConsole.WriteLine();
         }
 
-        private int askQuestion(string question)
+        private int AskQuestion(string question)
         {
             return AnsiConsole.Ask<int>(question,0);
         }
@@ -100,6 +140,11 @@ namespace Interactive
         {
             return table.AddColumn(name, col => col.Centered());
         }
-    }
 
+        private bool YESNO()
+        {
+            return AnsiConsole.Confirm("Do you wish to roll another task(y/n)?");
+            
+        }
+    }
 }
